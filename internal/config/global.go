004918231d63c072bf4977ced5ad3fa7184aab21
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2024-01-07 04:25:57 +0000

    fix(grpc/logging): log grpc errors correctly, update many deps

--- internal/config/global.go
+++ internal/config/global.go
@@ -1,43 +1,69 @@
 package config
 
 import (
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
+	"permission-service/internal/utils/runtime"
 	"strings"
 )
 
+const (
+	kafkaHostFlag   = "kafka-host"
+	kafkaPortFlag   = "kafka-port"
+	mongoDBURIFlag  = "mongodb-uri"
+	developmentFlag = "development"
+	grpcPortFlag    = "port"
+)
+
 type Config struct {
-	Kafka       *KafkaConfig
-	MongoDB     *MongoDBConfig
+	Kafka   KafkaConfig
+	MongoDB MongoDBConfig
+
 	Development bool
 
-	Port uint16
+	GRPCPort int
 }
 
 type KafkaConfig struct {
 	Host string
-	Port uint16
+	Port int
 }
 
 type MongoDBConfig struct {
 	URI string
 }
 
-func LoadGlobalConfig() (config *Config, err error) {
-	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-	viper.AutomaticEnv()
+func LoadGlobalConfig() Config {
+	viper.SetDefault(kafkaHostFlag, "localhost")
+	viper.SetDefault(kafkaPortFlag, 9092)
+	viper.SetDefault(mongoDBURIFlag, "mongodb://localhost:27017")
+	viper.SetDefault(developmentFlag, true)
+	viper.SetDefault(grpcPortFlag, 10010)
 
-	viper.SetConfigName("config")
-	viper.AddConfigPath(".")
+	pflag.String(kafkaHostFlag, viper.GetString(kafkaHostFlag), "Kafka host")
+	pflag.Int32(kafkaPortFlag, viper.GetInt32(kafkaPortFlag), "Kafka port")
+	pflag.String(mongoDBURIFlag, viper.GetString(mongoDBURIFlag), "MongoDB URI")
+	pflag.Bool(developmentFlag, viper.GetBool(developmentFlag), "Development mode")
+	pflag.Int32(grpcPortFlag, viper.GetInt32(grpcPortFlag), "gRPC port")
+	pflag.Parse()
 
-	err = viper.ReadInConfig()
-	if err != nil {
-		return
+	// Bind the viper flags to environment variables
+	viper.SetEnvKeyReplacer(strings.NewReplacer("-", "_"))
+	runtime.Must(viper.BindEnv(kafkaHostFlag))
+	runtime.Must(viper.BindEnv(kafkaPortFlag))
+	runtime.Must(viper.BindEnv(mongoDBURIFlag))
+	runtime.Must(viper.BindEnv(developmentFlag))
+	runtime.Must(viper.BindEnv(grpcPortFlag))
+
+	return Config{
+		Kafka: KafkaConfig{
+			Host: viper.GetString(kafkaHostFlag),
+			Port: int(viper.GetInt32(kafkaPortFlag)),
+		},
+		MongoDB: MongoDBConfig{
+			URI: viper.GetString(mongoDBURIFlag),
+		},
+		Development: viper.GetBool(developmentFlag),
+		GRPCPort:    int(viper.GetInt32(grpcPortFlag)),
 	}
-
-	err = viper.Unmarshal(&config)
-	if err != nil {
-		return
-	}
-
-	return
 }
--- internal/service/public.go
+++ internal/service/public.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/emortalmc/proto-specs/gen/go/grpc/permission"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -11,25 +12,32 @@ import (
 	"permission-service/internal/config"
 	"permission-service/internal/kafka/notifier"
 	"permission-service/internal/repository"
+	"permission-service/internal/utils/grpczap"
 	"sync"
 )
 
 func RunServices(ctx context.Context, logger *zap.SugaredLogger, wg *sync.WaitGroup, cfg *config.Config,
 	repo repository.Repository, notif notifier.Notifier) {
 
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.GRPCPort))
 	if err != nil {
 		logger.Fatalw("failed to listen", "error", err)
 	}
 
-	s := grpc.NewServer()
+	opts := []logging.Option{
+		logging.WithLogOnEvents(logging.StartCall, logging.FinishCall),
+	}
+
+	s := grpc.NewServer(grpc.ChainUnaryInterceptor(
+		logging.UnaryServerInterceptor(grpczap.InterceptorLogger(logger.Desugar()), opts...),
+	))
 
 	if cfg.Development {
 		reflection.Register(s)
 	}
 
 	permission.RegisterPermissionServiceServer(s, newPermissionService(logger, repo, notif))
-	logger.Infow("listening for gRPC requests", "port", cfg.Port)
+	logger.Infow("listening for gRPC requests", "port", cfg.GRPCPort)
 
 	go func() {
 		if err := s.Serve(lis); err != nil {
